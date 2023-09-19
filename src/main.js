cc539e1335bf825bbe3d931bf20b7d5eaef9c6ab
m77can <m77can@gmail.com>
2023-09-19 14:02:50 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from "vue";
 import "./styles.css";
 import App from "./App.vue";
-
+import { appWindow } from '@tauri-apps/api/window'
+document
+  .getElementById('titlebar-minimize')
+  .addEventListener('click', () => appWindow.minimize())
+document
+  .getElementById('titlebar-maximize')
+  .addEventListener('click', () => appWindow.toggleMaximize())
+document
+  .getElementById('titlebar-close')
+  .addEventListener('click', () => appWindow.close())
 createApp(App).mount("#app");
